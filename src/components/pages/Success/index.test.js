8e21d370c261f30fc1bc54456ea756e2d0851b0d
Gavyn McKenzie <gavyn@etchapps.com>
2020-02-14 14:13:47 +0000

    feat: Success tests

--- src/components/pages/Success/index.test.js
+++ src/components/pages/Success/index.test.js
@@ -5,10 +5,24 @@ import { mount } from 'enzyme';
 import Success from './index';
 
 import kerbside from './__mocks__/kerbside';
+import noKerbside from './__mocks__/no_kerbside';
 
 describe('Success', () => {
   beforeEach(() => {
     fetch.resetMocks();
+  });
+
+  it('should start in a loading state', () => {
+    const wrapper = mount(
+      <Success
+        loadRoute={() => {}}
+        locale="en"
+        postcode=""
+        material=""
+        token=""
+      />);
+
+    expect(wrapper.exists('.klw-loading')).toBe(true);
   });
 
   it('should render a title', (done) => {
@@ -23,10 +37,48 @@ describe('Success', () => {
       />);
 
     expect(fetch.mock.calls.length).toBe(1);
-    setTimeout(() => {
-      console.log(wrapper.html());
+    process.nextTick(() => {
+      wrapper.update();
       expect(wrapper.exists('h2')).toBe(true);
       done();
-    }, 1000);
+    });
+  });
+
+  it('should render a the kerbside block', (done) => {
+    fetch.mockResponseOnce(JSON.stringify(kerbside));
+    const wrapper = mount(
+      <Success
+        loadRoute={() => {}}
+        locale="en"
+        postcode=""
+        material=""
+        token=""
+      />);
+
+    process.nextTick(() => {
+      wrapper.update();
+      expect(wrapper.find('h2').text()).toBe('Good news!');
+      expect(wrapper.find('.klw-list__item').length).toBe(2);
+      done();
+    });
+  });
+
+  it('should render a the no kerbside block', (done) => {
+    fetch.mockResponseOnce(JSON.stringify(noKerbside));
+    const wrapper = mount(
+      <Success
+        loadRoute={() => {}}
+        locale="en"
+        postcode=""
+        material=""
+        token=""
+      />);
+
+    process.nextTick(() => {
+      wrapper.update();
+      expect(wrapper.find('h2').text()).toBe('Visit a local recycling location');
+      expect(wrapper.find('li').length).toBe(5);
+      done();
+    });
   });
 });

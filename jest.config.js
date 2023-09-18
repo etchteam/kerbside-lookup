68977febcf7653647e6f679d7c8326c712a84671
Josh Rose <josh@etch.co>
2023-09-18 17:20:08 +0100

    fix: get a working jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   testMatch: ['**/?(*.)test.js?(x)'],
   testEnvironment: 'jsdom',
-  moduleNameMapper: {
+  transform: {
     '\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['!node_modules/(?!enzyme-adapter-preact-pure/)'],
+  moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
